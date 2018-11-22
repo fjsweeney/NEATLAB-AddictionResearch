93c5ca83cf5fcc41da26e399487bffa27ec3cfe9
Ted Weber <webert3@wwu.edu>
2018-11-21 19:19:31 -0800

    New apprach for generating bags, much cleaner.

--- python/gen_bags.py
+++ python/gen_bags.py
@@ -1,10 +1,12 @@
 import argparse
 import os
+import pickle
 import pandas as pd
 import numpy as np
 
-from Models.Preprocessing import Bag
+from Models.Preprocessing.Bag import Bag
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 feature_set = ["activity_zscore", "cadence_zscore",
                "minute_ventilation_adjusted_zscore",
@@ -57,112 +59,15 @@ def generate_bags(sensor_df, smoking_df, bag_interval, pid):
         start_time += np.timedelta64(1, 'm')
         end_time += np.timedelta64(1, 'm')
 
+    print(" %d non-empty bags generated" % len(all_bags))
     return all_bags
 
 
-def split_train_test_bags(sensor_df, smoking_df, bag_interval):
-    """
-    Construct bags and split up sensor data into a train and test
-    set according to 'split_type'.
-
-    Args:
-        sensor_df:
-        smoking_df:
-        bag_interval:
-        split_type:
-
-    Returns:
-
-    """
-    train_bags, train_labels, test_bags, test_labels = ([] for i in range(4))
-
-    # Add time_of_day feature
-    # todo: Consider adding day_of_week feature
-    if "time_of_day" in feature_set:
-        scalar = MinMaxScaler()
-        sensor_df["time_of_day"] = sensor_df["datetime"].dt.hour
-        sensor_df["time_of_day"] = scalar.fit_transform(
-            np.reshape(sensor_df["time_of_day"].values, newshape=(-1, 1))
-        )
-
-    # Choose day to leave out to test generalization
-    study_days = np.unique(smoking_df["datetime"].dt.dayofyear.values)
-
-    for i in range(len(study_days)):
-        test_day = np.random.choice(study_days, replace=False)
-
-        # Make sure the test day contains at least one smoking episode
-        day_mask = (smoking_df["datetime"].dt.dayofyear == test_day)
-        test_day_subset = smoking_df.loc[day_mask]
-
-        if not test_day_subset.empty:
-            print("Date saved for test set: %s" %
-                  test_day_subset["datetime"].iloc[0].strftime("%m-%d-%Y"))
-
-            print("All smoking episodes (n=%d):" % (test_day_subset.shape[0]))
-            for datetime in test_day_subset["datetime"].values:
-                print("%s" % datetime.__str__())
-
-            break
-
-    # Iterate over all possible time windows and construct bags
-    start_time = sensor_df["datetime"].iloc[0]
-    end_time = start_time + np.timedelta64(bag_interval, 'm')
-    final_time = sensor_df.iloc[sensor_df.shape[0] - 1]["datetime"]
-    while start_time <= final_time:
-        # Extract subset between start and end times.
-        mask = ((sensor_df["datetime"] >= start_time) &
-                (sensor_df["datetime"] < end_time))
-
-        # Filter by time interval
-        subset = sensor_df.loc[mask]
-
-        # Construct bag
-        bag = subset[feature_set].values
-
-        # Check 'test_day' to determine what set the bag is a part of.
-        test_day_mask = (subset["datetime"].dt.dayofyear == test_day)
-        test_day_subset = subset.loc[test_day_mask]
-
-        # If majority of instances are on the 'test_day', put bag in test set.
-        if test_day_subset.shape[0] > int(bag_interval) / 2:
-            test_bags.append(bag)
-            test_labels.append(get_label(start_time, end_time, smoking_df))
-        else:
-            train_bags.append(bag)
-            train_labels.append(get_label(start_time, end_time, smoking_df))
-
-        # Shift window by one minute
-        start_time += np.timedelta64(1, 'm')
-        end_time += np.timedelta64(1, 'm')
-
-    return np.asarray(train_bags), np.asarray(train_labels), \
-           np.asarray(test_bags), np.asarray(test_labels)
-
-
-def filter_nans(bags, labels):
-    new_bags, new_labels = ([] for i in range(2))
-    for i, bag in enumerate(bags):
-        if not np.isnan(bag).any():
-            new_bags.append(bag)
-            new_labels.append(labels[i])
-
-    return np.asarray(new_bags), np.asarray(new_labels)
-
-
-def print_bag_stats(train_labels, test_labels, pid):
-    print("Participant %s - Training Data" % pid)
-    print("Number of Data Points: %d" % len(train_labels))
-    unique, counts = np.unique(train_labels, return_counts=True)
-    v_pct = float(counts[1]) / float(len(train_labels))
-    nv_pct = float(counts[0]) / float(len(train_labels))
-    print("Vulnerable: %.2f | Not Vulnerable: %.2f" % (v_pct, nv_pct))
-
-    print("Participant %s - Test Data" % pid)
-    print("Number of Data Points: %d" % len(test_labels))
-    unique, counts = np.unique(test_labels, return_counts=True)
-    v_pct = float(counts[1]) / float(len(test_labels))
-    nv_pct = float(counts[0]) / float(len(test_labels))
+def print_bag_stats(labels):
+    print("Number of Data Points: %d" % len(labels))
+    unique, counts = np.unique(labels, return_counts=True)
+    v_pct = float(counts[1]) / float(len(labels))
+    nv_pct = float(counts[0]) / float(len(labels))
     print("Vulnerable: %.2f | Not Vulnerable: %.2f" % (v_pct, nv_pct))
 
 
@@ -174,8 +79,10 @@ def main(args):
     participants = [filename for filename in os.listdir('./') if
                     filename.startswith('participant')]
 
+    # Construct bags for each participant
+    all_bags = []
     for participant in participants:
-        print("Creating bags for %s..." % participant)
+        print("Creating bags for %s..." % participant, end="")
         os.chdir(participant)
 
         pid = participant.split("_")[1]
@@ -190,25 +97,26 @@ def main(args):
                                  index_col=0)
         smoking_df["datetime"] = pd.to_datetime(smoking_df["datetime"])
 
-        train_bags, train_labels, test_bags, test_labels = \
-            split_train_test_bags(sensor_df, smoking_df, args.bag_interval)
-
-        print("\nData stats BEFORE NaN filtering...")
-        print_bag_stats(train_labels, test_labels, pid)
-
-        train_bags, train_labels = filter_nans(train_bags, train_labels)
-        test_bags, test_labels = filter_nans(test_bags, test_labels)
-
-        print("\nData stats AFTER NaN filtering...")
-        print_bag_stats(train_labels, test_labels, pid)
-
-        # Serialize arrays for future usage.
-        np.save("train_bags_%s.npy" % args.bag_interval, train_bags)
-        np.save("train_labels_%s.npy" % args.bag_interval, train_labels)
-        np.save("test_bags_%s.npy" % args.bag_interval, test_bags)
-        np.save("test_labels_%s.npy" % args.bag_interval, test_labels)
+        # Construct Bag objects
+        all_bags += generate_bags(sensor_df, smoking_df, args.bag_interval, pid)
 
         os.chdir("../")
+
+    print("%d bags generated in total." % len(all_bags))
+
+    train, test = train_test_split(all_bags, shuffle=True, test_size=0.2)
+
+    train_labels = [x.label for x in train]
+    test_labels = [x.label for x in test]
+
+    print("Train Data Stats:")
+    print_bag_stats(train_labels)
+    print("Test Data Stats:")
+    print_bag_stats(test_labels)
+
+    # todo: Pickle the data
+    pickle.dump(train, open("train_intv=%d_min.pkl" % args.bag_interval, "wb"))
+    pickle.dump(test, open("test_intv=%d_min.pkl" % args.bag_interval, "wb"))
 
     print('Done')
 
